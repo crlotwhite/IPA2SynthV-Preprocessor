080bdd0ce2501d66df0a9b3d4abf0595e08c0cd1
crlotwhite <crlotwhite@gmail.com>
2022-08-30 01:06:06 +0800

    Fix change json structure

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import csv,json
 
 res = {
-    "JPN": {},
-    "ENG": {},
-    "MAN": {},
+    "IPA": [],
+    "JPN": [],
+    "ENG": [],
+    "MAN": [],
 }
 
 with open('./IPA2SynthV.csv', encoding='utf-8') as f:
     # Example ['vowel', 'diphthong', 'ㅢ', 'xi', 'wi', '', '']
     rdr = csv.reader(f)
+    next(rdr)
     for row in rdr:
-        res["JPN"].update({row[3]: row[4]})
+        res["IPA"].append(row[3])
+        res["JPN"].append(row[4])
 
 with open("./output.json", 'w', encoding='utf-8') as file:
     json.dump(res, file, indent="\t", ensure_ascii=False)
